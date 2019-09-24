3f0ef43f3e08a5ff4875eb6841891f5a1c25ec0a
Dave Masino <davem@slalom.com>
2019-09-23 20:09:40 -0700

    Add a schedule

--- handler.py
+++ handler.py
@@ -1,5 +1,15 @@
-def run(event, context):
+import datetime
+import logging
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+def run(event, context):
+    current_time = datetime.datetime.now().time()
+    name = context.function_name
+    logger.info("Your cron function " + name + " ran at " + str(current_time))
+    
     return {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "event": event
