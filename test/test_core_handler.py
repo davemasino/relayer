4c3e033073c37a9fe1a53fab17cb799bb3b52861
Dave Masino <davem@slalom.com>
2020-04-02 20:58:26 -0700

    Some updates

--- test/test_core_handler.py
+++ test/test_core_handler.py
@@ -1,8 +1,8 @@
 from relayer.core import handler
 
+
 def test_handler():
-    cx = type('',(object,),{"function_name": "relayer-unit-test"})()
+    cx = type('', (object,), {"function_name": "relayer-unit-test"})()
     ret = handler(None, cx)
     msg = ret['message']
     assert msg == "Go Serverless v1.0! Your function executed successfully!"
-
